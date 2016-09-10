8d28aa06663c71f98c2871c2dbed9c3ba859cc00
Bede Kelly <bedekelly97@gmail.com>
2016-09-11 00:22:06 +0100

    Add a Lambda exception type

--- pluralizer.py
+++ pluralizer.py
@@ -15,8 +15,17 @@ O_EXCEPTIONS = [
 ]
 
 
-class BadRequest(Exception):
-    """Stub class to stop any bad request raising just a plain Exception."""
+class LambdaException(Exception):
+    """
+    Stub class to wrap all exceptions to be caught only by AWS Lambda
+    infrastructure.
+    """
+
+
+class BadRequest(LambdaException):
+    """
+    Represent a Bad Request error (i.e. an HTTP 400).
+    """
 
 
 def bad_request(message):
